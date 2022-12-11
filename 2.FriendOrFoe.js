249c850c1bef2d61aa48ef7db064ab4e38c724cb
diegobaena89 <diegobf.cello@gmail.com>
2022-12-10 21:30:12 -0300

    friendOrFoe challenge refactored

--- 2.FriendOrFoe.js
+++ 2.FriendOrFoe.js
@@ -1,20 +1,22 @@
+// Refactored Solution
 function friend(friends) {
   return friends.filter((trueFriend) => {
     return trueFriend.length === 4;
   });
 }
 
-console.log(friend(['Ryan', 'Kieran', 'Mark']));
+friend(['Ryan', 'Kieran', 'Mark']);
 
-// function friend(friends) {
-//   const trueFriend = [];
-//   for (let i = 0; i < friends.length; i++) {
-//     if (friends[i].length === 4) {
-//       trueFriend.push(friends[i]);
-//     }
-//   }
-//   console.log(trueFriend);
-//   return trueFriend;
-// }
+// Solution 1
+function friend(friends) {
+  const trueFriend = [];
+  for (let i = 0; i < friends.length; i++) {
+    if (friends[i].length === 4) {
+      trueFriend.push(friends[i]);
+    }
+  }
+  console.log(trueFriend);
+  return trueFriend;
+}
 
-// friend(['Ryan', 'Kieran', 'Mark']);
+friend(['Ryan', 'Kieran', 'Mark']);
