c4de1cd863f02979c75085a7dced7707979f6d35
diegobaena89 <diegobf.cello@gmail.com>
2022-12-19 19:07:01 -0300

    add stringEndsWith exercise with description

--- 3.StringEndsWIth.js
+++ 3.StringEndsWIth.js
@@ -1,3 +1,7 @@
+/*
+DESCRIPTION: Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+*/
+
 function solution(str, ending) {
   // TODO: complete
   return str.endsWith(ending);
