51ccc913ae125cb2cf6ade25e8d06918379b03f9
diegobaena89 <diegobf.cello@gmail.com>
2022-12-22 18:33:38 -0300

    add simplePigLatin challenge

---
+++ 9.SimplePigLatin.js
@@ -0,0 +1,25 @@
+/*
+Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+Examples
+
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+pigIt('Hello world !');     // elloHay orldway !
+*/
+
+function pigIt(str) {
+  //Code here
+  str = str.trim().split(/\s{1,}/);
+
+  return str
+    .map((val) => {
+      if (/^[A-Za-z]+$/.test(val)) {
+        return `${val.slice(1)}${val.slice(0, 1)}ay`;
+      }
+
+      return val;
+    })
+    .join(' ');
+}
+
+console.log(pigIt('Pig latin is cool'));
